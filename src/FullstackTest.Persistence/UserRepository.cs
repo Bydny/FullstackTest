bbf1f8a357cb9c12a01d968af5fbc722fbc77e27
Uladzislau Kurbatski <magicalvlad@gmail.com>
2020-01-15 15:57:42 +0300

    Added UserRepository

---
+++ src/FullstackTest.Persistence/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using FullstackTest.Domain;
+using FullstackTest.Persistence.Abstractions;
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullstackTest.Persistence
+{
+    public class UserRepository : EFRepository<User, Guid>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            return await DbSet.SingleOrDefaultAsync(x => x.Email == email);
+        }
+    }
+}
