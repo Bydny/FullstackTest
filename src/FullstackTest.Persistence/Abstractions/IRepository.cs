348086dbaad2dad80bd8914dcc1c7d1ca824dabf
Uladzislau Kurbatski <magicalvlad@gmail.com>
2020-01-15 11:34:47 +0300

    Added Persistence project and repos abstractions

---
+++ src/FullstackTest.Persistence/Abstractions/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FullstackTest.Persistence.Abstractions
+{
+    public interface IRepository<T, K> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(K id);
+        Task<T> CreateAsync(T item);
+        Task<T> UpdateAsync(T item);
+        Task DeleteAsync(K id);
+    }
+}
---
+++ src/FullstackTest.Persistence/Abstractions/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using FullstackTest.Domain;
+using System;
+using System.Threading.Tasks;
+
+namespace FullstackTest.Persistence.Abstractions
+{
+    public interface IUserRepository : IRepository<User, Guid>
+    {
+        public Task<User> GetByEmailAsync(string email);
+    }
+}
